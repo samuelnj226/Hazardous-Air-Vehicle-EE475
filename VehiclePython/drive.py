bac992b79d8b719d03cb50f957b4c734bb7b00e9
samuelnj226 <samuelnj@uw.edu>
2018-03-01 14:30:47 -0800

    delete comments

--- VehiclePython/drive.py
+++ VehiclePython/drive.py
@@ -58,37 +58,6 @@ def sigterm_handler(_signo, _stack_frame):
 signal.signal(signal.SIGTERM, sigterm_handler)
 signal.signal(signal.SIGINT, sigterm_handler)
 
-
-
-'''
-while (1)
-    y positive is forward
-    x positive is right
-
-    y from 200 to 1600 controls forward motion
-
-    # set forward speed
-    if y < 200
-        duty cycle right = 0
-        duty cycle left = 0
-    else if y > 1600
-        duty cycle right = 100
-        duty cycle left = 100
-    else
-        duty cycle right = y/1600
-        duty cycle left = y/1600
-
-    # set turn format
-
-    #right turn
-    if x > 200
-        duty cycle right = duty cycle right * (4000 - x)/4000
-    else if x < -200 #left turn
-        duty cycle left = duty cycle right * (4000 - x)/4000
-
-
-
-'''
 ser.readline()
 while 1:
     command = ser.readline()
